b55c0d3e28e9bf7f578f3bfe38fb71215ce25b19
Aditya Rai <adirai2901@gmail.com>
2025-09-09 22:16:13 +0530

    JAVA

---
+++ leetcode/_704_Binary_Search.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class _704_Binary_Search {
+    class Solution {
+    public int search(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]== target){
+                return mid;
+            }
+            else if(target>arr[mid]){
+                low = mid+1;
+            }
+            else if(target<arr[mid]){
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
+}
