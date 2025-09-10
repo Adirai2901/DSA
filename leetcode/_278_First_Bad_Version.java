7934445f1e82076b0edf3fe570830c8020988a36
Aditya Rai <adirai2901@gmail.com>
2025-09-10 23:11:34 +0530

    JAVA

---
+++ leetcode/_278_First_Bad_Version.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class _278_First_Bad_Version {
+    public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int low = 1, high = n;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (isBadVersion(mid)) {
+                high = mid; 
+            } else {
+                low = mid + 1; 
+            }
+        }
+        return low;
+    }
+}
+}
